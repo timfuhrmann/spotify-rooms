3bcd42842510846d4c62e94fe741f6250822ef70
timfuhrmann <tf@becklyn.com>
2021-06-07 22:36:45 +0200

    Fix logout

--- frontend/app/template/Template.tsx
+++ frontend/app/template/Template.tsx
@@ -13,6 +13,10 @@ const Stage = styled.div`
     width: 100%;
     height: 100%;
     overflow-y: scroll;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
 `;
 
 interface TemplateProps {
--- frontend/pages/api/logout.ts
+++ frontend/pages/api/logout.ts
@@ -3,10 +3,14 @@ import { APP_COOKIES } from "../../app/lib/util/api/Cookies";
 import { destroyCookie } from "nookies";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    destroyCookie({ res }, APP_COOKIES, {
-        httpOnly: true,
-        path: "/",
-    });
-
-    res.send("Successfully logged out.");
+    try {
+        destroyCookie({ res }, APP_COOKIES, {
+            httpOnly: true,
+            path: "/",
+        });
+        res.send("Successfully logged out.");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error trying to logout.");
+    }
 };
